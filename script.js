86a5b50bc4a0e3e9f5876a5efa96df90cc264303
SahrulGunawan10 <sg3533538@gmail.com>
2023-12-23 23:42:54 +0700

    fix form pop up

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ const formOpenBtn = document.querySelector("#form_open");
 const popup = document.querySelector(".popup");
 const formKeluhan = document.querySelector(".form_keluhan");
 const formCloseBtn = document.querySelector(".form_close");
+const overlay = document.querySelector(".overlay");
 
-formOpenBtn.addEventListener("click", () => popup.classList.add("show"));
-formCloseBtn.addEventListener("click", () => popup.classList.remove("show"));
+formOpenBtn.addEventListener("click", () => {
+    popup.classList.add("show");
+    overlay.style.display = "block";
+});
+formCloseBtn.addEventListener("click", () => {
+    popup.classList.remove("show");
+    overlay.style.display = "none";
+});
