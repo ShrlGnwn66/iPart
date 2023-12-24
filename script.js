c0d12fe556288477f37760fa4183796904989df1
SahrulGunawan10 <sg3533538@gmail.com>
2023-12-24 15:10:20 +0700

    history data keluhan

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //  Input Select
 $(document).ready(function () {
-    $("#nama_mitra").select2({
+    $("#nama_mitra, #pilih_mitra").select2({
         dropdownPosition: "below",
         minimumInputLength: 1,
     });
@@ -20,4 +20,45 @@ formOpenBtn.addEventListener("click", () => {
 formCloseBtn.addEventListener("click", () => {
     popup.classList.remove("show");
     overlay.style.display = "none";
+});
+
+
+// Pop Up Form
+const DataOpenBtn = document.querySelector("#data_open");
+const DataPopup = document.querySelector(".data_popup");
+const DataKeluhan = document.querySelector(".data_keluhan");
+const DataCloseBtn = document.querySelector(".data_close");
+
+DataOpenBtn.addEventListener("click", () => {
+    DataPopup.classList.add("show");
+    overlay.style.display = "block";
+});
+DataCloseBtn.addEventListener("click", () => {
+    DataPopup.classList.remove("show");
+    overlay.style.display = "none";
+});
+
+
+// Show Filter
+document.addEventListener('DOMContentLoaded', function () {
+    var filterButton = document.getElementById('toggleFilter');
+    var inputFilter = document.querySelector('.input_filter');
+    var arrowDown = document.querySelector('.arrow_down');
+    var arrowUp = document.querySelector('.arrow_up');
+
+
+    inputFilter.style.display = 'none';
+    arrowUp.style.display = 'none';
+
+    filterButton.addEventListener('click', function () {
+        if (inputFilter.style.display === 'none' || inputFilter.style.display === '') {
+            inputFilter.style.display = 'block';
+            arrowDown.style.display = 'none';
+            arrowUp.style.display = 'inline-block';
+        } else {
+            inputFilter.style.display = 'none';
+            arrowDown.style.display = 'inline-block';
+            arrowUp.style.display = 'none';
+        }
+    });
 });
