0868c9ac6803627c58a66c4c5712a55a8e0ebaf2
sylvaaditya <sylvaaditya80@gmail.com>
2023-12-24 19:04:00 +0700

    sweet alret,blade,validate form,

--- MitraCareHub/routes/web.php
+++ MitraCareHub/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+//home
+Route::get('/', [HomeController::class, 'index']);
+
+
+
+//FORM
+Route::POST('/welcome', [FormController::class, 'form']);
